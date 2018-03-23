20f004f46730d374e635359bdb9f18e055921a1f
Daniella Castro <danicastroaraujo@gmail.com>
2018-03-23 16:37:47 -0300

    Update number_users.py

--- number_users.py
+++ number_users.py
@@ -1,39 +1,27 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 import xml.etree.cElementTree as ET
-import pprint
-import re
-"""
-Your task is to explore the data a bit more.
-The first task is a fun one - find out how many unique users
-have contributed to the map in this particular area!
-
-The function process_map should return a set of unique user IDs ("uid")
-"""
-lower = re.compile(r'^([a-z]|_)*$')
 
 def get_user(element):
 
+    #Gets users id from an element 
+    #Arg: element: An element from the OpenStreetMap data
+    #Returns: users: A set of the user ids
+    
     users = set([])
     if 'uid' in element.keys():
         users.add(element.attrib['uid'])
     return users
-
+    
 
 def process_map(filename):
+    
+    #Parses document 
+    #Args: filename: OpenStreetMap data
+    #Returns: users: A set of the user ids
+    
     users = set([])
     for _, element in ET.iterparse(filename):
         users.update(get_user(element))
     return users
 
-
-def test():
-
-    users = process_map('rj_map.osm')
-    pprint.pprint(users)
-    print(len(users))
-
-
-
-if __name__ == "__main__":
-    test()
+users = process_map('rj_map.osm')
+print(len(users))
